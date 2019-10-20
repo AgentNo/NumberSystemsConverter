8ce5aa4e2a341b7fef5d81286fe7d307160f89c6
AgentNo <dfaulkner2010@hotmail.co.uk>
2019-10-20 14:41:54 +0100

    Added population commands, functionality for the exit button

--- projNumberSystemsConverter/frmMain.cs
+++ projNumberSystemsConverter/frmMain.cs
@@ -15,31 +15,47 @@ namespace projNumberSystemsConverter
         public frmMain()
         {
             InitializeComponent();
-        }
+            //Populate combo boxes with number systems
+            cboConvertFrom.Items.AddRange(new object[] { "Denary/Decimal", "Binary", "Octal", "Hexadecimal" });
+            cboConvertTo.Items.AddRange(new object[] { "Denary/Decimal", "Binary", "Octal", "Hexadecimal" });
+        }//frmMain()
 
         private void btnConvert_Click(object sender, EventArgs e)
         {
 
-        }
+        }//btnConvert_Click
 
         private void btnClear_Click(object sender, EventArgs e)
         {
 
-        }
+        }//btnClear_Click
 
         private void btnHelp_Click(object sender, EventArgs e)
         {
 
-        }
+        }//btnHelp_Click
 
         private void btnExplanation_Click(object sender, EventArgs e)
         {
 
-        }
+        }//btnExplanation_Click
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            // When this button is clicked, the form will close. First, display a message box to the user asking to confirm their choice
 
-        }
-    }
-}
+            DialogResult dialogResult = MessageBox.Show("Close the application?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                //close the form
+                this.Close();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //otherwise continue
+                txtConvertFrom.Focus();
+            }
+        }//btnExit_Click
+    }//class
+}//namespace
